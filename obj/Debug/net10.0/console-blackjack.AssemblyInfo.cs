29bf37ce1328d9c39e5c4b15c33a0c11673926e4
vít Holejšovský <vit.holejsovsky@academicschool.cz>
2026-05-27 10:27:24 +0200

    Made README.md

--- obj/Debug/net10.0/console-blackjack.AssemblyInfo.cs
+++ obj/Debug/net10.0/console-blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("console-blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e2b376406beef9eefe4e9c8b9a786dc45ad056f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+267addbc77f1a6ec7afdea8ffbfdc4f1ee53c57b")]
 [assembly: System.Reflection.AssemblyProductAttribute("console-blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("console-blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
