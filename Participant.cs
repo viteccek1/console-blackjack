23a78b6c5ff73f56e49735b090e3434af97f2525
vít Holejšovský <vit.holejsovsky@academicschool.cz>
2026-06-08 13:32:37 +0200

    Edited Player.cs and added Participant.cs

--- Participant.cs
+++ Participant.cs
@@ -1,6 +1,54 @@
-namespace console_blackjack;
-
-public class Participant
+public abstract class Participant
 {
+    public string Name { get; private set; }
+    public List<Card> Hand { get; private set; }
     
+    public Participant(string name)
+    {
+        Name = name;
+        Hand = new List<Card>();
+    }
+    
+    public void AddCard(Card card)
+    {
+        Hand.Add(card);
+    }
+
+    public int CalculateScore()
+    {
+        int score = 0;
+        int aceCount = 0;
+        
+        foreach (Card card in Hand)
+        {
+            score += card.Value;
+            if (card.Rank == "A")
+            {
+                aceCount++;
+            }
+        }
+        
+        while (score > 21 && aceCount > 0)
+        {
+            score -= 10; 
+            aceCount--; 
+        }
+
+        return score;
+    }
+    
+    public void ShowHand()
+    {
+        Console.Write($"{Name} má v ruce: ");
+        foreach (Card card in Hand)
+        {
+            Console.Write($"{card} ");  
+        }
+        Console.WriteLine($"Celkem bodů: {CalculateScore()}");
+    }
+    
+    public void ClearHand()
+    {
+        Hand.Clear();
+    }
 }
--- Player.cs
+++ Player.cs
@@ -1,57 +1,7 @@
-public class Player
+
+public class Player : Participant
 {
-    
-    public string Name { get; private set; }
-    public List<Card> Hand { get; private set; }
-    
-    public Player(string name)
+    public Player(string name) : base(name)
     {
-        Name = name;
-        Hand = new List<Card>();
-    }
-    
-    public void AddCard(Card card)
-    {
-        Hand.Add(card);
-    }
-
-    
-    public int CalculateScore()
-    {
-        int score = 0;
-        int aceCount = 0;
-        
-        foreach (Card card in Hand)
-        {
-            score += card.Value;
-            if (card.Rank == "A")
-            {
-                aceCount++;
-            }
-        }
-        
-        // Pokud hráč kvůli Esu překročil 21, snížíme hodnotu Esa z 11 na 1 bod
-        while (score > 21 && aceCount > 0)
-        {
-            score -= 10; 
-            aceCount--; 
-        }
-
-        return score;
-    }
-    
-    public void ShowHand()
-    {
-        Console.Write($"{Name} má v ruce: ");
-        foreach (Card card in Hand)
-        {
-            Console.Write($"{card} ");  
-        }
-        Console.WriteLine($"Celkem bodů: {CalculateScore()}");
-    }
-    
-    public void ClearHand()
-    {
-        Hand.Clear();
     }
 }
